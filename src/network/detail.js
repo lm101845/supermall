34307ae3cd10c3bb58ab6fadeafcb6669cfc79f6
lm101845 <1018457683@qq.com>
2021-01-01 19:37:43 +0800

    商品参数信息的展示

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,56 @@ export function getDetail(iid) {
         }
     })
 }
+
+export class Goods{ 
+    // ES6里面可以直接定义class
+    // 导出一个类
+    constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title
+    this.desc = itemInfo.desc
+    this.newPrice = itemInfo.price
+    this.oldPrice = itemInfo.oldPrice
+    this.discount = itemInfo.discountDesc
+    this.columns = columns
+    this.services = services
+    this.realPrice = itemInfo.lowNowPrice
+  }
+}
+
+// const g = new Goods()
+// 最终g里面就保存了所有的数字
+// g.title,g.desc等
+
+
+
+// 补充知识：类
+// class Person { 
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// const p = new Person('why',18)
+
+export class Shop {
+// 虽然数据是在一个里面，但是整体上来看还是稍微有些复杂，所以也弄了一个类集中存放数据
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.name = shopInfo.name;
+    this.fans = shopInfo.cFans;
+    this.sells = shopInfo.cSells;
+    this.score = shopInfo.score;
+    this.goodsCount = shopInfo.cGoods
+  }
+}
+
+export class GoodsParam {
+    // 商品的规格等数据
+  constructor(info, rule) {
+    // 注: images可能没有值(某些商品有值, 某些没有值)
+    this.image = info.images ? info.images[0] : '';
+    this.infos = info.set;
+    this.sizes = rule.tables;
+  }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -3,8 +3,15 @@
     <div id="detail">
         <!-- <nav-bar><div slot="center">详情页</div></nav-bar> -->
         <!-- 这个导航还是稍微有些复杂的，那就不要在这里做了 -->
-        <detail-nav-bar/>
-        <detail-swiper :top-images="topImages"/>
+        <detail-nav-bar class="detail-nav"/>
+        <scroll class="content" ref="scroll">
+            <!-- scroll必须要有固定的高度 -->
+            <detail-swiper :top-images="topImages"/>
+            <detail-base-info :goods="goods"></detail-base-info>
+            <detail-shop-info :shop="shop"></detail-shop-info>
+            <detail-goods-info :detail-info='detailInfo'></detail-goods-info>
+            <detail-param-info :param-info="paramInfo"></detail-param-info>
+        </scroll>
     </div>
 </template>
 
@@ -12,8 +19,14 @@
 // import NavBar from 'components/common/navbar/NavBar'
 import DetailNavBar from './childComps/DetailNavBar'
 import DetailSwiper from './childComps/DetailSwiper'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
+import DetailShopInfo from './childComps/DetailShopInfo'
+import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+import DetailParamInfo from './childComps/DetailParamInfo'
 
-import {getDetail} from 'network/detail'
+import Scroll from 'components/common/scroll/Scroll'
+
+import {getDetail,Goods,Shop,GoodsParam} from 'network/detail'
 
 export default {
     name: 'Detail',
@@ -21,12 +34,23 @@ export default {
     components:{
     //NavBar
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    Scroll
     },
     data(){
         return {
           iid: null,
-          topImages:[]
+          topImages:[],
+        //   goods:null
+        //   写空的话会报错，因为默认default是object
+          goods:{},
+          shop:{},
+          detailInfo:{},
+          paramInfo:{}
         }
     },
     created(){
@@ -41,16 +65,54 @@ export default {
             console.log(res)
             // this.res = res
             // 不要向上面这样做,要分离一下
-
+            const data = res.result;
             // 1.获取顶部的图片轮播数据
-            this.topImages = res.result.itemInfo.topImages
+            // this.topImages = res.result.itemInfo.topImages
+            this.topImages = data.itemInfo.topImages
 
             // 接下来我们用轮播图对得到的数据进行展示
+            // 2.获取商品信息
+            this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+            // 3.创建店铺信息的对象
+            this.shop = new Shop(data.shopInfo)
+
+            // 4.获取商品详细信息
+            this.detailInfo = data.detailInfo
+
+            // 5.获取参数信息
+            this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
         })
+    },
+    methods:{
+        imageLoad(){
+            this.$refs.scroll.refresh()
+        }
     }
 }
 </script>
 
 <style scoped>
+    #detail{
+        position: relative;
+        z-index: 9;
+        background-color: #fff;
+        /* 详情页很多地方是透明的，所以并没有完全盖上，给它加个背景色就好了 */
+        height: 100vh;
+    }
 
+    .content{
+        /* 如何给它固定高度有2种方式 */
+        /* 一种是使用cacul给它进行计算 */
+        /* 另一种方式是通过定位 */
+        height: calc(100% - 44px);
+    }
+
+    .detail-nav{
+        position: relative;
+        /* 能用相对定位就用相对定位，不要用绝对定位 */
+        /* 因为相对定位它依然保持在自己原来的位置，只是占着茅坑不拉屎 */
+        z-index: 9;
+        background-color: #fff;
+    }
 </style>
