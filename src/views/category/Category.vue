945f08d8bcd2abcd6a93ce586df526ad0f8583e8
lm101845 <1018457683@qq.com>
2021-02-03 11:11:13 +0800

    分类和我的模块还没写

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="category">
-    分类，老师没讲了，我以后再写
+    分类，老师没讲了，我以后写。
+    <br />
+    这个项目还要自己再写一次的。
   </div>
 </template>
 
 <script>
-  export default {
-		name: "Category",
-  
-	}
+export default {
+  name: "Category"
+};
 </script>
 
-<style scoped>
-  
-</style>
+<style scoped></style>
