8f0fb2dc6d868417cd233e2339cde152b72b6535
lm101845 <1018457683@qq.com>
2021-02-03 11:39:27 +0800

    demo

--- vue.config.js
+++ vue.config.js
@@ -21,5 +21,6 @@ module.exports = {
         }
     },
     lintOnSave: false,
-    publicPath:  "./"
+    publicPath: process.env.NODE_ENV=== 'development' ? './' : '././',
+    outputDir:'dist'
 }
